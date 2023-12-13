a316264b8042679cf56f5cbb4719e74ebbf74646
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2023-12-13 15:31:40 +0800

    Initial Commit

---
+++ check-in-box-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  check-in-box-ios
+//
+//  Created by Yi-Chin Hsu on 2023/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
