f4b79ae4d5713e48398d1513d29b5c6bc880d2e5
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2023-12-15 20:36:46 +0800

    [feat] Add pick-a-question feature

--- Features/Sources/Features/ClassicCheckInFeature.swift
+++ Features/Sources/Features/ClassicCheckInFeature.swift
@@ -9,16 +9,20 @@ import ComposableArchitecture
 import SwiftUI
 
 public struct ClassicCheckInFeature: Reducer {
-  public struct State: Equatable {}
+  public struct State: Equatable {
+    var questions: [String] = []
+  }
 
   public enum Action: Equatable {
-    case tap
+    case pickButtonTapped
   }
 
   public var body: some ReducerOf<Self> {
-    Reduce { _, action in
+    Reduce { state, action in
       switch action {
-      case .tap:
+      case .pickButtonTapped:
+        // TODO: Performance improvement
+        state.questions = state.questions.shuffled()
         return .none
       }
     }
@@ -27,9 +31,34 @@ public struct ClassicCheckInFeature: Reducer {
 
 struct ClassicCheckInView: View {
   let store: StoreOf<ClassicCheckInFeature>
+
   var body: some View {
-    WithViewStore(self.store, observe: { $0 }) { _ in
-      Text("Classic")
+    WithViewStore(self.store, observe: { $0 }) { store in
+      VStack {
+        Spacer()
+        
+        Text(store.questions.first ?? "")
+          .multilineTextAlignment(.center)
+          .font(.title)
+          .bold()
+        
+        Spacer()
+
+        VStack {
+          Button {
+            store.send(.pickButtonTapped)
+          } label: {
+            Label("換一題", systemImage: "arrow.counterclockwise")
+              .font(.headline)
+              .foregroundColor(.black)
+              .frame(maxWidth: .infinity)
+              .padding(.vertical, 16)
+              .background(.white)
+              .clipShape(Capsule())
+          }
+        }
+      }
+      .padding()
     }
   }
 }
@@ -38,10 +67,21 @@ struct ClassicCheckInView: View {
   NavigationStack {
     ClassicCheckInView(
       store: Store(
-        initialState: ClassicCheckInFeature.State()
+        initialState: ClassicCheckInFeature.State(
+          questions: [
+            "身上使用最久的東西是什麼？",
+            "最喜歡的一部電影？",
+            "今年最期待的一件事情？",
+            "我不為人知的一個奇怪技能",
+            "做過最像大人的事情",
+            "今年最快樂的回憶",
+            "最想再去一次的國家/城市",
+          ]
+        )
       ) {
         ClassicCheckInFeature()
       }
     )
   }
+  .preferredColorScheme(.dark)
 }
