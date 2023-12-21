48c84300eeb0d32df07ee178c19eae0b433e0f39
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2023-12-21 23:18:31 +0800

    [refactor] Move `UserSettingsFeatureTests` to single file

--- Features/Tests/FeaturesTests/FeaturesTests.swift
+++ Features/Tests/FeaturesTests/FeaturesTests.swift
@@ -50,22 +50,3 @@ extension XCTestCase {
     ]
   }
 }
-
-@MainActor
-final class UserSettingsFeatureTests: XCTestCase {
-  func test_openURL_navigateToFeedbackForm() async {
-    let store = TestStore(
-      initialState: SettingsFeature.State(),
-      reducer: { SettingsFeature() }
-    ) {
-      $0.openURL = OpenURLEffect(
-        handler: { url in
-          XCTAssertEqual(url, URL(string: "https://forms.gle/Vr4MjtowWPxBxr5r9")!)
-          return true
-        }
-      )
-    }
-
-    await store.send(.sendFeedbackButtonTapped)
-  }
-}
