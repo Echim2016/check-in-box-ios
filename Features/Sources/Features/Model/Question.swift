4eb6944d10a31e8bf32d1f2e816bcb78721ad188
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2023-12-21 18:51:50 +0800

    [feat] Add data model `Question`

---
+++ Features/Sources/Features/Model/Question.swift
@@ -0,0 +1,11 @@
+//
+//  Question.swift
+//
+//
+//  Created by Yi-Chin Hsu on 2023/12/21.
+//
+
+public struct Question: Identifiable, Codable {
+  public let id: String
+  public let question: String
+}
