1f8c11801f22cd7c83623d378e0b3f75d2c3206b
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2023-12-21 13:19:18 +0800

    [feat] Add test for opening feedback form URL

--- Features/Tests/FeaturesTests/FeaturesTests.swift
+++ Features/Tests/FeaturesTests/FeaturesTests.swift
@@ -99,3 +99,22 @@ final class ModelListFeatureTests: XCTestCase {
     }
   }
 }
+
+@MainActor
+final class UserSettingsFeatureTests: XCTestCase {
+  func test_openURL_navigateToFeedbackForm() async {
+    let store = TestStore(
+      initialState: SettingsFeature.State(),
+      reducer: { SettingsFeature() }
+    ) {
+      $0.openURL = OpenURLEffect(
+        handler: { url in
+          XCTAssertEqual(url, URL(string: "https://forms.gle/Vr4MjtowWPxBxr5r9")!)
+          return true
+        }
+      )
+    }
+    
+    await store.send(.sendFeedbackButtonTapped)
+  }
+}
