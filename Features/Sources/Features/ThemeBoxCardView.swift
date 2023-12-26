e6782014b92ddc3b4471b8d54af9960173a2aa2c
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2023-12-26 17:48:47 +0800

    [feat] Add `ThemeBoxCardView`

---
+++ Features/Sources/Features/ThemeBoxCardView.swift
@@ -0,0 +1,54 @@
+//
+//  ThemeBoxCardView.swift
+//
+//
+//  Created by Yi-Chin Hsu on 2023/12/26.
+//
+
+import SwiftUI
+
+public struct ThemeBoxCardView: View {
+  let title: String
+  let subtitle: String
+  let url: URL?
+
+  public init(title: String, subtitle: String, url: URL? = nil) {
+    self.title = title
+    self.subtitle = subtitle
+    self.url = url
+  }
+
+  public var body: some View {
+    VStack {
+      Text(subtitle)
+        .font(.subheadline)
+        .foregroundStyle(.secondary)
+
+      Text(title)
+        .font(.title2)
+        .fontWeight(.semibold)
+        .foregroundStyle(.primary)
+    }
+    .padding()
+    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+    .background {
+      NetworkImage(url: url)
+        .blur(radius: 1)
+        .overlay(
+          Color.black.opacity(0.3)
+        )
+    }
+  }
+}
+
+struct NetworkImage: View {
+  let url: URL?
+  var body: some View {
+    AsyncImage(url: url) { image in
+      image
+        .resizable()
+        .aspectRatio(contentMode: .fill)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    } placeholder: {}
+  }
+}
