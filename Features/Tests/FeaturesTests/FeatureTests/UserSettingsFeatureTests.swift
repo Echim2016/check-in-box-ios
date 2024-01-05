5e5532ec46159704c7e14ec7f53260bacc8bb4ba
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2024-01-05 17:16:30 +0800

    [feat] Add tests for gift card activation key when submitted

--- Features/Tests/FeaturesTests/FeatureTests/UserSettingsFeatureTests.swift
+++ Features/Tests/FeaturesTests/FeatureTests/UserSettingsFeatureTests.swift
@@ -1,6 +1,6 @@
 //
 //  UserSettingsFeatureTests.swift
-//  
+//
 //
 //  Created by Yi-Chin Hsu on 2023/12/21.
 //
@@ -17,21 +17,21 @@ final class UserSettingsFeatureTests: XCTestCase {
     arrange(store, toAssert: .clickSettingsPgFeedbackFormBtn(parameters: [:]))
     await store.send(.sendFeedbackButtonTapped)
   }
-  
+
   func test_openURL_navigateToAuthorProfile() async {
     let store = makeSUT()
     arrange(store, toAssert: .authorProfileUrl)
     arrange(store, toAssert: .clickSettingsPgAuthorProfileBtn(parameters: [:]))
     await store.send(.authorProfileButtonTapped)
   }
-  
+
   func test_shareButton_trackEventWhenTapped() async {
     let store = makeSUT()
     arrange(store, toAssert: .clickSettingsPgShareBtn(parameters: [:]))
     XCTAssertEqual(store.state.shareLinkUrl, .shareLinkUrl)
     await store.send(.shareButtonTapped)
   }
-  
+
   func test_redeemGiftCardButton_presentGiftCardInoutBoxPage() async {
     let store = makeSUT()
     arrange(store, toAssert: .clickSettingsPgGiftCardBtn(parameters: [:]))
@@ -42,15 +42,49 @@ final class UserSettingsFeatureTests: XCTestCase {
 }
 
 extension UserSettingsFeatureTests {
-  func makeSUT() -> TestStoreOf<SettingsFeature> {
+  func test_giftCardInputBoxPage_validActivationKeySubmitted() async {
+    let activationKey = "valid_key"
+    let store = makeSUT(
+      state: SettingsFeature.State(
+        presentGiftCardInputBoxPage: InputBoxFeature.State(
+          activationKey: activationKey
+        )
+      )
+    )
+    arrange(store, toAssert: activationKey)
+
+    await store.send(.presentGiftCardInputBoxPage(.presented(.activateButtonTapped)))
+    await store.receive(.presentGiftCardInputBoxPage(.presented(.activationKeySubmitted(activationKey)))) { state in
+      state.presentGiftCardInputBoxPage = nil
+      state.hapticFeedbackTrigger = true
+    }
+  }
+  
+  func test_giftCardInputBoxPage_emptyActivationKeySubmitted() async {
+    let activationKey = ""
+    let store = makeSUT(
+      state: SettingsFeature.State(
+        presentGiftCardInputBoxPage: InputBoxFeature.State(
+          activationKey: activationKey
+        )
+      )
+    )
+    arrange(store, toAssert: activationKey)
+
+    await store.send(.presentGiftCardInputBoxPage(.presented(.activateButtonTapped)))
+  }
+}
+
+extension UserSettingsFeatureTests {
+  func makeSUT(state: SettingsFeature.State = SettingsFeature.State()) -> TestStoreOf<SettingsFeature> {
     TestStore(
-      initialState: SettingsFeature.State(),
+      initialState: state,
       reducer: { SettingsFeature() }
     ) {
       $0.firebaseTracker = FirebaseTracker(logEvent: { _ in })
     }
   }
-  
+
   func arrange(
     _ store: TestStoreOf<SettingsFeature>,
     toAssert destinationUrl: URL
@@ -62,7 +96,7 @@ extension UserSettingsFeatureTests {
       }
     )
   }
-  
+
   func arrange(
     _ store: TestStoreOf<SettingsFeature>,
     toAssert event: FirebaseEvent
@@ -70,6 +104,21 @@ extension UserSettingsFeatureTests {
     store.dependencies.firebaseTracker = FirebaseTracker(
       logEvent: { trackingEvent in
         XCTAssertEqual(trackingEvent, event)
+      }
+    )
+  }
+  
+  func arrange(
+    _ store: TestStoreOf<SettingsFeature>,
+    toAssert activationKey: String
+  ) {
+    store.dependencies.giftCardAccessManager = GiftCardAccessManager(
+      isFullAccess: { key in
+        XCTAssertEqual(activationKey, key)
+        return true
+      },
+      setAccess: { key in
+        XCTAssertEqual(activationKey, key)
       }
     )
   }
