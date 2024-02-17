547247bd4745650ba79010ec711fedc23e0f17c3
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2024-02-17 10:53:43 +0800

    [refactor] Apply latest macro to settings page

--- Features/Sources/Features/SettingsFeature.swift
+++ Features/Sources/Features/SettingsFeature.swift
@@ -9,10 +9,12 @@ import ComposableArchitecture
 import StoreKit
 import SwiftUI
 
-public struct SettingsFeature: Reducer {
+@Reducer
+public struct SettingsFeature {
+  @ObservableState
   public struct State: Equatable {
-    @PresentationState var presentDebugModeInputBoxPage: InputBoxFeature.State?
-    @PresentationState var presentInAppWebViewPage: InAppWebFeature.State?
+    @Presents var presentDebugModeInputBoxPage: InputBoxFeature.State?
+    @Presents var presentInAppWebViewPage: InAppWebFeature.State?
     var feedbackFormUrl: URL = .feedbackFormUrl
     var authorProfileUrl: URL = .authorProfileUrl
     var authorProfileImageUrl: URL? = .authorProfileImageUrl
@@ -37,7 +39,6 @@ public struct SettingsFeature: Reducer {
   @Dependency(\.openURL) var openURL
   @Dependency(\.debugModeManager) var debugModeManager
   @Dependency(\.firebaseTracker) var firebaseTracker
-  
 
   public var body: some ReducerOf<Self> {
     Reduce { state, action in
@@ -74,12 +75,12 @@ public struct SettingsFeature: Reducer {
       case .submitAppReviewButtonTapped:
         firebaseTracker.logEvent(.clickSettingsPgSubmitAppReviewBtn(parameters: [:]))
         return .none
-        
+
       case let .presentDebugModeInputBoxPage(.presented(.activationKeySubmitted(key))):
         state.presentDebugModeInputBoxPage = nil
         debugModeManager.setAccess(key)
         return .none
-        
+
       case .presentDebugModeInputBoxPage:
         return .none
 
@@ -95,7 +96,7 @@ public struct SettingsFeature: Reducer {
         return .none
       }
     }
-    .ifLet(\.$presentDebugModeInputBoxPage, action: /Action.presentDebugModeInputBoxPage) {
+    .ifLet(\.$presentDebugModeInputBoxPage, action: \.presentDebugModeInputBoxPage) {
       InputBoxFeature()
     }
   }
@@ -106,99 +107,97 @@ struct SettingsView: View {
   @Environment(\.requestReview) var requestReview
 
   var body: some View {
-    WithViewStore(self.store, observe: { $0 }) { store in
-      List {
-        Section {
-          Button {} label: {
-            ShareLink(item: store.state.shareLinkUrl) {
-              /// Problem: Slow loading issue after tapping the share link without any UI indication
-              /// Solution: Implement wide label for better pressed state indication
-              HStack {
-                Label("分享給朋友", systemImage: "square.and.arrow.up")
-                  .foregroundStyle(.white)
-                Spacer()
-                  .frame(minWidth: .leastNonzeroMagnitude)
-              }
-            }
-            .simultaneousGesture(
-              TapGesture().onEnded {
-                store.send(.shareButtonTapped)
-              }
-            )
-          }
-
-          Button {
-            store.send(.sendFeedbackButtonTapped)
-          } label: {
-            Label("回饋真心話", systemImage: "paperplane")
-              .foregroundStyle(.white)
-          }
-
-          Button {
-            store.send(.submitQuestionsButtonTapped)
-          } label: {
-            Label("我想出一題", systemImage: "lightbulb")
-              .foregroundStyle(.white)
-          }
-          
-          Button {
-            store.send(.submitAppReviewButtonTapped)
-            requestReview()
-          } label: {
-            Label("評價五顆星", systemImage: "star")
-              .foregroundStyle(.white)
-          }
-        } header: {
-          Text("服務")
-        }
-
-        Section {
-          Button {
-            store.send(.authorProfileButtonTapped)
-          } label: {
-            ProfileCellView(url: store.state.authorProfileImageUrl)
-          }
-          .padding(.vertical, 2)
-
-        } header: {
-          Text("作者")
-        }
-
-        if store.debugModeButtonEnabled {
-          Section {
-            Button {
-              store.send(.debugModeButtonTapped)
-            } label: {
-              Label("Debug Mode", systemImage: "wrench.adjustable")
+    List {
+      Section {
+        Button {} label: {
+          ShareLink(item: store.state.shareLinkUrl) {
+            /// Problem: Slow loading issue after tapping the share link without any UI indication
+            /// Solution: Implement wide label for better pressed state indication
+            HStack {
+              Label("分享給朋友", systemImage: "square.and.arrow.up")
                 .foregroundStyle(.white)
+              Spacer()
+                .frame(minWidth: .leastNonzeroMagnitude)
             }
-          } header: {
-            Text("Debug")
           }
+          .simultaneousGesture(
+            TapGesture().onEnded {
+              store.send(.shareButtonTapped)
+            }
+          )
         }
 
-        if let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String,
-           let buildNumber = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String
-        {
-          Section {
-            Text("v\(version) (\(buildNumber))")
-              .foregroundStyle(.secondary)
-              .onTapGesture(count: 10) {
-                store.send(.debugModeButtonEnabled)
-              }
-          } header: {
-            Text("版本")
+        Button {
+          store.send(.sendFeedbackButtonTapped)
+        } label: {
+          Label("回饋真心話", systemImage: "paperplane")
+            .foregroundStyle(.white)
+        }
+
+        Button {
+          store.send(.submitQuestionsButtonTapped)
+        } label: {
+          Label("我想出一題", systemImage: "lightbulb")
+            .foregroundStyle(.white)
+        }
+
+        Button {
+          store.send(.submitAppReviewButtonTapped)
+          requestReview()
+        } label: {
+          Label("評價五顆星", systemImage: "star")
+            .foregroundStyle(.white)
+        }
+      } header: {
+        Text("服務")
+      }
+
+      Section {
+        Button {
+          store.send(.authorProfileButtonTapped)
+        } label: {
+          ProfileCellView(url: store.state.authorProfileImageUrl)
+        }
+        .padding(.vertical, 2)
+
+      } header: {
+        Text("作者")
+      }
+
+      if store.debugModeButtonEnabled {
+        Section {
+          Button {
+            store.send(.debugModeButtonTapped)
+          } label: {
+            Label("Debug Mode", systemImage: "wrench.adjustable")
+              .foregroundStyle(.white)
           }
+        } header: {
+          Text("Debug")
         }
       }
-      .onAppear {
-        store.send(.trackViewSettingsPageEvent)
+
+      if let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String,
+         let buildNumber = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String
+      {
+        Section {
+          Text("v\(version) (\(buildNumber))")
+            .foregroundStyle(.secondary)
+            .onTapGesture(count: 10) {
+              store.send(.debugModeButtonEnabled)
+            }
+        } header: {
+          Text("版本")
+        }
       }
+    }
+    .onAppear {
+      store.send(.trackViewSettingsPageEvent)
     }
     .sheet(
       store: self.store.scope(
         state: \.$presentDebugModeInputBoxPage,
-        action: { .presentDebugModeInputBoxPage($0) }
+        action: \.presentDebugModeInputBoxPage
       )
     ) { inputBoxViewStore in
       InputBoxView(store: inputBoxViewStore)
@@ -207,7 +206,7 @@ struct SettingsView: View {
     .fullScreenCover(
       store: self.store.scope(
         state: \.$presentInAppWebViewPage,
-        action: { .presentInAppWebViewPage($0) }
+        action: \.presentInAppWebViewPage
       )
     ) { webViewStore in
       InAppWebView(store: webViewStore)
